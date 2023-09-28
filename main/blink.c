2bffc1c4cd3cdf3a3f550724da70e8e85714f6fa
Jalisa Blanda <141678968+nbdbucyu231@users.noreply.github.com>
2023-09-28 08:58:34 +0000

    initial

--- main/blink.c
+++ main/blink.c
@@ -1,40 +1,31 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
+/* 可以使用项目配置菜单（idf.py menuconfig）选择要闪烁的GPIO，
+   或者您可以编辑以下行并在此处设置一个数字。
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_GPIO 2
 
 void app_main(void)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
+    /* 配置BLINK_GPIO引脚的IOMUX寄存器（一些引脚在复位时已经
+       被复用为GPIO，但有些默认为其他功能并需要切换到GPIO。
+       请参阅技术参考以获取引脚及其默认功能的列表。）
     */
     gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
+    /* 将GPIO设置为推挽输出 */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-	printf("Turning off the LED\n");
+    while (1)
+    {
+        /* 熄灭LED（输出低电平）*/
+        printf("关闭LED\n");
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-	printf("Turning on the LED\n");
+        /* 点亮LED（输出高电平）*/
+        printf("打开LED\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
